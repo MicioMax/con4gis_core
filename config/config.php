723e50998cf78d46b66e56d84633e78362907b74
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-08-05 16:01:11 +0200

    nxt snapshot

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@
  */
 
 $GLOBALS['con4gis_core_extension']['installed'] = true;
-$GLOBALS['con4gis_core_extension']['version']   = '1.8.2-snapshot';
+$GLOBALS['con4gis_core_extension']['version']   = '1.9.1-snapshot';
 $GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.9';
 
 // API-Registration
