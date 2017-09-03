2cf14fc130b650bce90b1beb1d4342fb4fbdded0
Massimo Del Fedele <max@veneto.com>
2017-09-03 10:50:03 +0200

    Added codesnipped and image2 plugins to ckeditor

--- assets/vendor/ckeditor/config.js
+++ assets/vendor/ckeditor/config.js
@@ -34,12 +34,13 @@ CKEDITOR.editorConfig = function( config ) {
 	config.removeButtons = 'Underline,Subscript,Superscript';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	config.format_tags = 'p;h1;h2;h3;h4;h5;h6;pre;address;div';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 
-	config.extraPlugins = 'justify,fileUpload,bbcode,panelbutton,floatpanel,colorbutton,blockquote,youtube,smiley,sourcedialog';
+	config.removePlugins = 'bbcode';
+	config.extraPlugins = 'justify,fileUpload,panelbutton,floatpanel,colorbutton,blockquote,youtube,smiley,sourcedialog,image2,codesnippet';
 	config.image_previewText = ' ';
-
+	config.autoParagraph = false;
 };
